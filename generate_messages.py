41e8dfede8d1d539e9f5b4385c97c396c02ba270
Aidan Taha <botatooo@protonmail.com>
2023-05-05 21:26:47 -0400

    feat: add readline for up arrow functionality

--- generate_messages.py
+++ generate_messages.py
@@ -1,4 +1,7 @@
+import readline
 import markovify
+
+input_history = []
 
 with open("data/model.json", encoding="utf-8") as f:
     print("Fetching saved Markov chain...")
@@ -17,7 +20,9 @@ def generate_messages(count):
 def main():
     while True:
         try:
-            count = int(input("How many messages do you want to generate? "))
+            count = input("How many messages do you want to generate? ")
+            input_history.append(count)
+            count = int(count)
         except Exception:
             count = 1
 
@@ -25,4 +30,6 @@ def main():
             print(">", message)
 
 if __name__ == "__main__":
+    readline.set_history_length(100)
+    readline.set_completer(lambda: input_history[-1])
     main()
